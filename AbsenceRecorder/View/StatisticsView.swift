8543f4a7e9d72f80fd9c9662f589f382a713701f
HersovJasper <74298714+HersovJasper@users.noreply.github.com>
2021-03-09 19:20:46 +0000

    Part 3 Completed

---
+++ AbsenceRecorder/View/StatisticsView.swift
@@ -0,0 +1,22 @@
+//
+//  StatisticsView.swift
+//  AbsenceRecorder
+//
+//  Created by Jasper Hersov on 09/03/2021.
+//
+
+import SwiftUI
+
+struct StatisticsView: View {
+    @EnvironmentObject var state: StateController
+    
+    var body: some View {
+        Text("Statistics View")
+    }
+}
+
+struct StatisticsView_Previews: PreviewProvider {
+    static var previews: some View {
+        StatisticsView()
+    }
+}
